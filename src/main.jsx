edff92769497ac7ab370ae62c7fded34a227d0d7
Meldak <meldak.z@gmail.com>
2025-06-20 00:21:12 -0600

    Adding Info to Card

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,10 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div id='ChurchView' >
       <InfoCard
-        name="Título de Ejemplo"
-        description="Este es un ejemplo de descripción que se mostrará u ocultará cuando hagas click en el ícono." 
+        name="Capilla Jesús del Río"
+        description="Un tesoro escondido en el corazón de Tlaxcala
+Situada en una tranquila esquina del centro histórico, la Capilla Jesús del Río es una joya poco conocida que ofrece una experiencia íntima con el pasado colonial de Tlaxcala. Aunque modesta en tamaño, su arquitectura de influencia renacentista y su cúpula sobre el crucero la convierten en un espacio singular y armonioso.
+Esta capilla ha sido un punto de reunión espiritual durante siglos, y su atmósfera serena invita a la contemplación y la pausa. El interior, sencillo pero acogedor, conserva una sensación de recogimiento difícil de encontrar en otras construcciones más concurridas." 
         />
       <PortView />
     </div>
