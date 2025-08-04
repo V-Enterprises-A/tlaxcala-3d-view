abb061a26094770adc923d4d300cdaef806e60d1
arnulfo <arnulfovazgom74@gmail.com>
2025-08-03 20:23:46 -0600

    iglesia de pocito arriba

--- src/Churchs/models/C5.jsx
+++ src/Churchs/models/C5.jsx
@@ -2,7 +2,41 @@ import React from 'react'
 
 function C2({ nodes, materials, ...props }) {
     return (
-        <></>
+        <group {...props} dispose={null}>
+            <group position={[0, 0, 2.853]} rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+                <mesh geometry={nodes.Mesh.geometry} material={materials.Ladrillo___Hexagonal_2} />
+                <mesh geometry={nodes.Mesh_1.geometry} material={materials.Pintura__Oroen_Beige} />
+                <mesh geometry={nodes.Mesh_2.geometry} material={materials.Pintura__Gris_Claro} />
+                <mesh geometry={nodes.Mesh_3.geometry} material={materials.Madera___Pino_Granulado_Horizontal} />
+                <mesh geometry={nodes.Mesh_4.geometry} material={materials.Metal___Acero_Inoxidable} />
+                <mesh geometry={nodes.Mesh_5.geometry} material={materials.Metal___Hierro} />
+                <mesh geometry={nodes.Mesh_6.geometry} material={materials.Pintura__Arena_Beige} />
+                <mesh geometry={nodes.Mesh_7.geometry} material={materials.Ladrillo___Viejo_Natural} />
+                <mesh geometry={nodes.Mesh_8.geometry} material={materials.Hormig_n___02} />
+                <mesh geometry={nodes.Mesh_9.geometry} material={materials.Ladrillo___Aparejo_Com_n} />
+                <mesh geometry={nodes.Mesh_10.geometry} material={materials.Hormig_n___04} />
+                <mesh geometry={nodes.Mesh_11.geometry} material={materials.Hormig_n___04_1} />
+                <mesh geometry={nodes.Mesh_12.geometry} material={materials.Vidrio___Claro_R_pido} />
+                <mesh geometry={nodes.Mesh_13.geometry} material={materials.Madera___Caoba_Horizontal} />
+                <mesh geometry={nodes.Mesh_14.geometry} material={materials.Estuco___Blanco_Fino} />
+                <mesh geometry={nodes.Mesh_15.geometry} material={materials.Madera___Caoba_Vertical} />
+                <mesh geometry={nodes.Mesh_16.geometry} material={materials.Piedra___Caliza_Fina} />
+                <mesh geometry={nodes.Mesh_17.geometry} material={materials.Baldosas___Tostado_30x30} />
+                <mesh geometry={nodes.Mesh_18.geometry} material={materials.Madera___Nogal_Vertical} />
+                <mesh geometry={nodes.Mesh_19.geometry} material={materials.Pintura__Negro_Marfil} />
+                <mesh geometry={nodes.Mesh_20.geometry} material={materials.Pintura__Cadmio_Naranja} />
+                <mesh geometry={nodes.Mesh_21.geometry} material={materials.Revestimiento___Medio} />
+                <mesh geometry={nodes.Mesh_22.geometry} material={materials.Pavimento___Asfalto_Claro} />
+                <mesh geometry={nodes.Mesh_23.geometry} material={materials.Cer_mico___Porcelana} />
+                <mesh geometry={nodes.Mesh_24.geometry} material={materials.Pintura__Azul_Royal} />
+                <mesh geometry={nodes.Mesh_25.geometry} material={materials.Pintura__Blanco_Satinado} />
+                <mesh geometry={nodes.Mesh_26.geometry} material={materials.Ladrillo___Aparejo_Flamenco} />
+                <mesh geometry={nodes.Mesh_27.geometry} material={materials.Pintura__Titanio_Blanco} />
+                <mesh geometry={nodes.Mesh_28.geometry} material={materials.Baldosas___Blanco_Mate_15x15} />
+                <mesh geometry={nodes.Mesh_29.geometry} material={materials.Pintura__Gris_Oscuro} />
+            </group>
+            <mesh geometry={nodes.muroLogos.geometry} material={materials['M_muroLogos.002']} position={[0.35, 0, -4.052]} rotation={[Math.PI / 2, 0, 0]} scale={1.345} />
+        </group>
     )
 }
 
